41f684d78c3e58a9f0dfdb75daf3d85bde2f66eb
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-10-26 20:21:09 +0100

    Updates

---
+++ precache-manifest.b3a422f7c6fd99fece9c78d28be5c314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40e5e86ec4e200ef72fbd0c58d7f13c0",
+    "url": "/React-Calculator/static/media/numbers.40e5e86e.jpg"
+  },
+  {
+    "revision": "4a9b6dfe2c0ed41fbe05",
+    "url": "/React-Calculator/static/js/runtime~main.4a9b6dfe.js"
+  },
+  {
+    "revision": "0343ef21d18f04e78738",
+    "url": "/React-Calculator/static/js/main.0343ef21.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/React-Calculator/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "0343ef21d18f04e78738",
+    "url": "/React-Calculator/static/css/main.1334332d.chunk.css"
+  },
+  {
+    "revision": "b477ae5f6e0182ab825a3b3cc8cbd52d",
+    "url": "/React-Calculator/index.html"
+  }
+];
