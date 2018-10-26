1c27f99adcd03de689fd805192ee6f5a9482daae
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-10-26 14:29:55 +0100

    add decimal support and fix bugs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ class Body extends React.Component {
   state = {
     value: [],
     answer: '',
+    operatorDisabled: false,
   }
 
   handleClick = (e) => {
@@ -15,13 +16,24 @@ class Body extends React.Component {
     this.setState({ value: this.state.value });
   }
 
-  handleClickEquals = () => {
-    const inputArray = this.state.value;
-    if (inputArray.length === 0) {
-      this.setState({ answer: 0 })
+  handleClickOperator = (e) => {
+    if (this.state.operatorDisabled === true) {
+      const noInput = '';
+      this.state.value.push(noInput);
+      this.setState({ value: this.state.value });
+    } else {
+      this.setState({ operatorDisabled: true })
+      const userInput1 = e.currentTarget.dataset.id;
+      this.state.value.push(userInput1);
+      this.setState({ value: this.state.value });
     }
+  }
+
+  handleClickEquals = () => {
+    this.setState({ operatorDisabled: false });
+    const inputArray = this.state.value;
     const stringInput = inputArray.join('');
-    const regex = /\D/;
+    const regex = /[^\d.]/;
     const operatorIndex = stringInput.search(regex);
     const operator = regex.exec(stringInput);
     const number1 = Number(stringInput.slice(0, operatorIndex));
@@ -29,15 +41,15 @@ class Body extends React.Component {
     if ((operatorIndex === 0 && operator[0] === "*") || (operatorIndex === 0 && operator[0] === "/") || (operatorIndex === stringInput.length - 1)) {
       this.setState({ answer: "error: incomplete expression" });
     } else if (operatorIndex === -1) {
-      this.setState({ answer: stringInput })
+      this.setState({ answer: stringInput + " = " + stringInput })
     } else if (operator[0] === "+") {
-      this.setState({ answer: (number1 + number2) })
+      this.setState({ answer: stringInput + " = " + (number1 + number2) })
     } else if (operator[0] === "-") {
-      this.setState({ answer: (number1 - number2) })
+      this.setState({ answer: stringInput + " = " + (number1 - number2) })
     } else if (operator[0] === "*") {
-      this.setState({ answer: (number1 * number2) })
+      this.setState({ answer: stringInput + " = " + (number1 * number2) })
     } else if (operator[0] === "/") {
-      this.setState({ answer: (number1 / number2) })
+      this.setState({ answer: stringInput + " = " + (number1 / number2) })
     } else {
       this.setState({ answer: '' })
     }
@@ -71,9 +83,15 @@ class Body extends React.Component {
   handleClickBack = () => {
     this.setState({ answer: 0 })
     const inputArray4 = this.state.value;
-    const stringInput4 = inputArray4.pop();
-    console.log(stringInput4);
-    this.setState({ value: inputArray4 });
+    const inputString4 = inputArray4.join('')
+    const lastDigit = inputArray4.pop();
+    const regex = /[^\d.]/;
+    const operator = regex.exec(inputString4);
+    if (operator === undefined) {
+      this.setState({ value: inputArray4 });
+    } else {
+     this.setState({ operatorDisabled: false});
+    }
   }
 
   render() {
@@ -90,34 +108,34 @@ class Body extends React.Component {
             <td className='display-answer' colSpan='4'>{this.state.answer}</td>
           </tr>
           <tr className='row'>
-            <td className='item' data-id='%' onClick={this.handleClickPercent}>%</td>
-            <td className='item' onClick={this.handleClickSquare}>x²</td>
-            <td className='item' onClick={this.handleClickRoot}>√x</td>
-            <td className='item' onClick={this.handleClickBack}>⌫</td>
+            <td className='operator' onClick={this.handleClickPercent}>%</td>
+            <td className='operator' onClick={this.handleClickSquare}>x²</td>
+            <td className='operator' onClick={this.handleClickRoot}>√x</td>
+            <td className='item, delete' onClick={this.handleClickBack}>⌫</td>
           </tr>
           <tr className='row'>
             <td className='item' data-id='7' onClick={this.handleClick}>7</td>
             <td className='item' data-id='8' onClick={this.handleClick}>8</td>
             <td className='item' data-id='9' onClick={this.handleClick}>9</td>
-            <td className='item' data-id='-' onClick={this.handleClick}>-</td>
+            <td className='operator' data-id='-' onClick={this.handleClickOperator}>-</td>
           </tr>
           <tr className='row'>
             <td className='item' data-id='4' onClick={this.handleClick}>4</td>
             <td className='item' data-id='5' onClick={this.handleClick}>5</td>
             <td className='item' data-id='6' onClick={this.handleClick}>6</td>
-            <td className='item' data-id='+' onClick={this.handleClick}>+</td>
+            <td className='operator' data-id='+' onClick={this.handleClickOperator}>+</td>
           </tr>
           <tr className='row'>
             <td className='item' data-id='1' onClick={this.handleClick}>1</td>
             <td className='item' data-id='2' onClick={this.handleClick}>2</td>
             <td className='item' data-id='3' onClick={this.handleClick}>3</td>
-            <td className='item' data-id='*' onClick={this.handleClick}>×</td>
+            <td className='operator' data-id='*' onClick={this.handleClickOperator}>×</td>
           </tr>
           <tr className='row'>
             <td className='item' data-id='0' onClick={this.handleClick}>0</td>
             <td className='item' data-id='.' onClick={this.handleClick}>.</td>
-            <td className='item' data-id='/' onClick={this.handleClick}>÷</td>
-            <td className='item' data-id='=' onClick={this.handleClickEquals}>=</td>
+            <td className='operator' data-id='/' onClick={this.handleClickOperator}>÷</td>
+            <td className='equals' data-id='=' onClick={this.handleClickEquals}>=</td>
           </tr>
         </tbody>
       </table>
